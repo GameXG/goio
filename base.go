145548aeaf84f2f2555d49d6710f97ea562c036b
GameXG <GameXG@Gmail.Com>
2016-12-10 23:23:59 +0800

    新增 write 版本的 NopCloser

--- base.go
+++ base.go
@@ -10,3 +10,13 @@ type WriteFlushCloser interface {
 	Flusher
 	io.Closer
 }
+type nopCloser struct {
+	io.Writer
+}
+
+func (nopCloser) Close() error { return nil }
+
+
+func NopCloser(w io.Writer) io.WriteCloser{
+	return NopCloser(w)
+}
