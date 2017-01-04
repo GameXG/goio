8ddf66f0512249732847d70dd18de01fc70afd33
GameXG <GameXG@Gmail.Com>
2017-01-04 20:12:17 +0800

    增加 WriteAll

--- base.go
+++ base.go
@@ -14,9 +14,25 @@ type nopCloser struct {
 	io.Writer
 }
 
-func (nopCloser) Close() error { return nil }
+func (nopCloser) Close() error {
+	return nil
+}
 
-
-func NopCloser(w io.Writer) io.WriteCloser{
+func NopCloser(w io.Writer) io.WriteCloser {
 	return nopCloser{w}
+}
+
+func WriteAll(w io.Writer, buf []byte) (n int,err error) {
+	b := buf
+	for len(b) != 0 {
+		ln,lerr:=w.Write(b)
+
+		n+=ln
+		b = b[ln:]
+
+		if lerr!=nil{
+			return n,lerr
+		}
+	}
+	return
 }
---
+++ base_test.go
@@ -0,0 +1,26 @@
+package goio
+
+import "testing"
+
+type testWriteAll struct {
+	n int
+}
+
+func (t*testWriteAll)Write(b []byte)(int,error)  {
+	if len(b)<10{
+		return len(b),nil
+	}
+	return 10,nil
+}
+
+func TestWriteAll(t *testing.T) {
+	tw:=testWriteAll{}
+	buf:=make([]byte,9998)
+	n,err:=WriteAll(&tw,buf)
+	if n!=len(buf){
+		t.Error(n,"!=",len(buf))
+	}
+	if err!=nil{
+		t.Error(err)
+	}
+}
